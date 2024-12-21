744dfb19e191fed8383604d5ad02a10ce3edb64b
Ákos Buzás <buzas.akos@telekom.hu>
2024-12-21 01:04:21 +0100

    gomoku board styling

--- src/app/gomoku/amoba-game/amoba-game.component.ts
+++ src/app/gomoku/amoba-game/amoba-game.component.ts
@@ -1,22 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-amoba-game',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './amoba-game.component.html',
   styleUrl: './amoba-game.component.scss'
 })
 export class AmobaGameComponent {
 
+  board: string[][] = [];
+  currentPlayer: string = 'X';
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    
+    this.board = Array.from({ length: 25 }, () =>
+      Array.from({ length: 25 }, () => '')
+    );
   }
 
   onHomeClicked() {
     this.router.navigateByUrl('gomoku');
+  }
+
+  makeMove(row: number, col: number): void {
+    // Make a move only if the cell is empty
+    if (!this.board[row][col]) {
+      this.board[row][col] = this.currentPlayer;
+      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X'; // Switch player
+    }
   }
 }
